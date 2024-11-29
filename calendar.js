67935df0f056bf76efec3ccb4e9480d1103faecc
RogelioGio <giotalingdan@gmail.com>
2024-11-29 14:25:34 +0800

    add:Calendar Changes

--- calendar.js
+++ calendar.js
@@ -5,7 +5,7 @@ let month = date.getMonth();
 const day = document.querySelector(".calendar-dates");
 
 const currDate = document.querySelector(".calendar-current-date");
-const prenexIcons = document.querySelectorAll(".calendar-navigation span");
+const prenexIcons = document.querySelectorAll(".calendar-navigation i");
 const months  = [
     "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
 ];
